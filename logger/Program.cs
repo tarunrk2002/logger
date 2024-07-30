6a8d17f82f6c6aa3d76ce42e77a5dc121db90a22
Scita <Scita@DESKTOP-4LR6HHJ>
2024-07-30 14:38:56 +0530

    ....

--- logger/Program.cs
+++ logger/Program.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Configuration;
 
 namespace logger
 {
     internal class Program
     {
         static void Main(string[] args)
-        {
+        { 
         }
     }
 }
---
+++ logger/logging.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace logger
+{
+    internal static class logging
+    {
+        public static void WriteLog() 
+        {
+            string logPath = ConfigurationManager.AppSettings["path_log"];
+            
+        }    
+    }
+}
